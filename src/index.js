7654120479b7df1a6ff8f269329d43ed4fed7d2c
FreekAlberti <federicoalberi96@gmail.com>
2020-12-16 20:54:37 +0100

    added onclick code refactoring correct key problem

--- src/index.js
+++ src/index.js
@@ -5,24 +5,32 @@ import "./index.css"
 // Books vars
 const books = [
   {
+    id: 1,
     img: "https://getsirv.com/wp-content/uploads/2020/11/book-of-black-cover-closed.png",
     title: "The Gray Book",
-    author: "Dr Gray"
+    author: "Dr Gray",
+    pages: 123456
   },
   {
+    id: 2,
     img: "https://cdn.pixabay.com/photo/2014/04/03/10/50/book-311432_640.png",
     title: "The Red Book",
-    author: "Dr Red"
+    author: "Dr Red",
+    pages: 12345
   },
   {
+    id: 3,
     img: "https://www.pngkit.com/png/full/204-2048179_big-blue-book-free-clipart-blue-book-clip.png",
     title: "The Blue Book",
-    author: "Dr Blue"
+    author: "Dr Blue",
+    pages: 1234
   },
   {
+    id: 4,
     img: "https://www.clker.com/cliparts/4/f/r/e/K/G/purple-book.svg",
     title: "The Purple Book",
-    author: "Dr Purple"
+    author: "Dr Purple",
+    pages: 123
   },
 ];
 
@@ -31,20 +39,23 @@ function BookList() {
     <section className="booklist_flex">
         {books.map((book)=>{
           return (
-            <Book book = {book}></Book>
+            <Book key = {book.id} {...book}></Book>
           );
         })}
     </section>
   );
 }
 
-const Book = (props) => {
-  const { img, title, author } = props.book;
+const Book = ({ img, title, author, pages }) => {
+  const clickHandler = (print) => {
+    alert("Number of pages: " + print);
+  }
   return (
-    <article className="article_book">
+    <article className="article_book" onMouseOver={() => {console.log(author)}}>
       <img src={img} alt="Book image" className="img_book"/>
       <h1 className="title_book">{title.toUpperCase()}</h1>
       <h3 style={{ fontFamily: "Ubuntu, sans-serif"}}>{author}</h3>
+      <button type="button" onClick={() => clickHandler(pages)}>Click me</button>
     </article>
   );
 }
